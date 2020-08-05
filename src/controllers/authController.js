88565e3df08f3c7747d1a4439ea353389fe228ca
savio777 <saviopf22@gmail.com>
2020-08-04 22:47:08 -0300

    criação do token no login e cadastrar

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const authJson = require("../config/auth.json");
 
 const User = require("../models/user");
 
@@ -14,9 +17,19 @@ router.post("/register", async (req, res) => {
     }
 
     const user = await User.create(req.body);
+
+    if (!user) {
+      return res.status(400).send({ error: "Error in signup" });
+    }
+
+    const token = jwt.sign({ id: user.id }, authJson.secret, {
+      expiresIn: 86400,
+    });
+
+    // esconder a senha do usuario que está no banco de dados
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({ user, token });
   } catch (error) {
     console.log("err ", error);
 
@@ -27,22 +40,33 @@ router.post("/register", async (req, res) => {
 router.post("/authenticate", async (req, res) => {
   const { email, password } = req.body;
 
-  // select na senha pq está configurado para não mostrar por padrão nas buscas no banco
-  const user = await User.findOne({ email }).select("+password");
+  try {
+    // select na senha pq está configurado para não mostrar por padrão nas buscas no banco
+    const user = await User.findOne({ email }).select("+password");
 
-  if (!user) {
-    return res.status(400).send({ error: "User not found" });
+    if (!user) {
+      return res.status(400).send({ error: "User not found" });
+    }
+
+    //const hash = await bcrypt.hashSync(password, 10);
+    //const hash = await bcrypt.compare(password, user.password);
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(400).send({ error: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, authJson.secret, {
+      expiresIn: 86400,
+    });
+
+    user.password = undefined;
+
+    res.send({ user, token });
+  } catch (error) {
+    console.log("err ", error);
+
+    return res.status(400).send({ error: "Auth failed" });
   }
-
-  //const hash = await bcrypt.hashSync(password, 10);
-
-  if (!(await bcrypt.compare(password, user.password))) {
-    return res.status(400).send({ error: "Invalid password" });
-  }
-
-  user.password = undefined;
-
-  res.send({ user });
 });
 
 module.exports = (app) => app.use("/auth", router);
